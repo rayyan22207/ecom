c367c4beb7d99fa8426cdbaa799c8fc225f1fb96
rayyan22207 <rayyan22207@gmail.com>
2025-07-20 01:28:52 +0500

    admin

--- src/comments/admin.py
+++ src/comments/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("product", "user_email", "rating", "is_approved", "created_at")
+    list_filter = ("is_approved", "rating", "created_at")
+    search_fields = ("user__email", "product__name", "comment")
+    readonly_fields = ("created_at",)
+
+    def user_email(self, obj):
+        return obj.user.email if obj.user else "Guest"
+    user_email.short_description = "User Email"
