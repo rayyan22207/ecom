26a0efea31f5b2569ac2a33fd60a986def7f1a0a
rayyan22207 <rayyan22207@gmail.com>
2025-07-29 15:44:44 +0500

    updating

--- src/cart/urls.py
+++ src/cart/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import CartRetrieveView, CartItemAddView, CartItemDeleteView, CartItemUpdateView
+from .api_views import CartRetrieveView, CartItemAddView, CartItemDeleteView, CartItemUpdateView
+from . import views
 
 urlpatterns = [
     path('', CartRetrieveView.as_view(), name='cart-detail'),
